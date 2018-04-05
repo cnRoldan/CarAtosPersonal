00cbdc4c7ba16a6138d02c74fe7ca04f71546d52
Roldan Ramos <claudio-nicolas.roldanramos.external@atos.net>
2018-04-05 12:14:16 +0100

    Creo obj ruta para pasar por params

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef} from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var google;
 @Component({
   selector: 'perfil-home',
   templateUrl: 'perfil.html'
 })
 export class perfilPage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+
   constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+
+  loadMap(){
+
+
+    let origeLng = new google.maps.LatLng(28.241449902908233, -16.837590280950963);
+
+    let mapOptions = {
+      center: origeLng,
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
 
   }
 
