c70a19518502e5711e70d248f78fa29d5d6acfd5
Mattiaa95 <mattiaa95@gmail.com>
2018-04-09 16:14:12 +0100

    segment

--- src/pages/nuevaRuta/nuevaRuta.ts
+++ src/pages/nuevaRuta/nuevaRuta.ts
@@ -1,49 +1,19 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 // import { Day } from '../../classes/Day';
 
-declare var google;
 @Component({
   selector: 'page-nuevaRuta',
   templateUrl: 'nuevaRuta.html'
 })
 export class nuevaRutaPage {
-
-   @ViewChild('map') mapElement: ElementRef;
-   map: any;
-
+  selected:string="L";
+  dias:string[]=["L","M","X","J","V"];
 
    constructor(public navCtrl: NavController) {
 
    }
 
-   ionViewDidLoad(){
-     this.loadMap();
-   }
-
-   loadMap(){
-
-     let origeLng = new google.maps.LatLng(28.241449902908233, -16.837590280950963);
-     let destinoLng = new google.maps.LatLng(28.126584746441665, -16.56268612105714);
-     const TRAVELMODE='DRIVING';
-
-     let mapOptions = {
-       center: origeLng,
-       zoom: 10,
-       mapTypeId: google.maps.MapTypeId.ROADMAP
-     }
-
-     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-
-      let directionsService:any=new google.maps.DirectionsService;
-
-     directionsService.route({
-        origin: origeLng,
-        destination: destinoLng,
-        travelMode: google.maps.TravelMode[TRAVELMODE]
-      });
-
-   }
 
 
 
