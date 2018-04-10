6d6d8226615b85ec574b8d4f95ffb2761d2bfe0a
Roldan Ramos <claudio-nicolas.roldanramos.external@atos.net>
2018-04-10 15:24:08 +0100

    Pantalla detalles

--- src/pages/detalles-ruta/detalles-ruta.ts
+++ src/pages/detalles-ruta/detalles-ruta.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Day } from '../../classes/Day';
+
 
 @IonicPage()
 @Component({
@@ -7,6 +9,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'detalles-ruta.html',
 })
 export class DetallesRutaPage {
+
+  selected:string="L";
+
+  lunes = new Day("L");
+  martes = new Day("M");
+  miercoles = new Day("X");
+  jueves = new Day("J");
+  viernes = new Day("V");
+
+  diasSemana:Day[] = [
+    this.lunes,
+    this.martes,
+    this.miercoles,
+    this.jueves,
+    this.viernes
+  ];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
